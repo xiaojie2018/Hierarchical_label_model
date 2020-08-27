889278ead9d0457199ef662ce3911079b50c5366
xiaojie <xiaojie201206@126.com>
2020-08-27 17:44:21 +0800

    init

---
+++ src/logging.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+# author: xiaojie
+# datetime: 2020/8/27 17:27
+# software: PyCharm
+
+from __future__ import absolute_import
+import logging
+
+logger = logging.getLogger()
+
+
+def init_logger(log_file=None, log_file_level=logging.NOTSET):
+    log_format = logging.Formatter("[%(asctime)s %(levelname)s] %(message)s")
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setGormatter(log_format)
+    logger.handlers = [console_handler]
+
+    if log_file and log_file != "":
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(log_file_level)
+        file_handler.setFormatter(log_format)
+        logger.addHandler(file_handler)
+
+    return logger
+
